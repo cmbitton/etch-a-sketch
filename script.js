1ae835d4316fe2413fdbeeed283e4767cb25cdf2
Curtis <cmbitton@gmail.com>
2022-10-26 10:16:46 -0400

    Add basic logic
    Added basic logic to fill game
    board and start sketches

---
+++ script.js
@@ -0,0 +1,31 @@
+function createDrawingArea(size) {
+    const board = document.querySelector('.drawing-board');
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        for (let j = 0; j < size; j++) {
+            const pixel = document.createElement('div');
+            console.log(pixel);
+            pixel.classList.add('pixel');
+            row.append(pixel);
+        }
+        board.append(row);
+    }
+}
+
+function draw(){
+    const pixels = document.querySelectorAll('.pixel');
+    for(const pixel of pixels){
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'white';
+        })
+    }
+}
+function cancelDraw(){
+    const pixels = document.querySelectorAll('.pixel');
+    for(const pixel of pixels){
+        pixel.outerHTML = pixel.outerHTML;
+    }
+}
+createDrawingArea(16);
+window.addEventListener('mousedown', draw);
+window.addEventListener('mouseup', cancelDraw);
